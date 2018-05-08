cf3bf4a7fc17fd6d12c14b1b9b0638a43641224b
astikute <agnese.stikute@gmail.com>
2018-05-08 21:34:07 +0300

    Day 3
    Funkcijas

--- Day_3_1/Day_3_1/Program.cs
+++ Day_3_1/Day_3_1/Program.cs
@@ -13,7 +13,15 @@ namespace Day_3_1
             Funkcija3(6, 7);
 
             int a = Funkcija4(100, 1);
-            Console.WriteLine(a);
+            Console.WriteLine(a); //Var ari Console.WriteLine(Funkcija4(100,1))
+
+            Console.Write("Ievadiet 1.vardu: ");
+            string name1 = Console.ReadLine();
+            Console.Write("Ievadiet 2.vardu: ");
+            string name2 = Console.ReadLine();
+            Console.Write("Ievadiet 3.vardu: ");
+            string name3 = Console.ReadLine();
+            Console.WriteLine(Funkcija5(name1, name2, name3));
 
             Console.ReadLine();
         }
@@ -27,6 +35,11 @@ namespace Day_3_1
         static int Funkcija4 (int skaitlis1, int skaitlis2)
         {
             return skaitlis1 + skaitlis2;
+        }
+
+        static string Funkcija5 (string vards1, string vards2, string vards3)
+        {
+            return vards1 + " " + vards2 + " " + vards3;
         }
 
     }
