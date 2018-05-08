ff88e1ee5ec072ef1bf94fa959691b9fbef767ab
astikute <agnese.stikute@gmail.com>
2018-05-08 20:36:02 +0300

    Funkcija4
    Funkcija ar parametriem

--- Day_3_1/Day_3_1/Program.cs
+++ Day_3_1/Day_3_1/Program.cs
@@ -10,6 +10,25 @@ namespace Day_3_1
     {
         static void Main(string[] args)
         {
+            Funkcija3(6, 7);
+
+            int a = Funkcija4(100, 1);
+            Console.WriteLine(a);
+
+            Console.ReadLine();
         }
+
+        static void Funkcija3(int skaitlis1, int skaitlis2)
+        {
+            int c = skaitlis1 + skaitlis2;
+            Console.WriteLine(c);
+        }
+
+        static int Funkcija4 (int skaitlis1, int skaitlis2)
+        {
+            return skaitlis1 + skaitlis2;
+        }
+
     }
+
 }
